fe3d84e05cbd44d00fc07518a4bca6144670a98b
gannavaramanvesh-netizen <gannavaramanvesh@gmail.com>
2025-10-18 17:24:21 +0530

    Implement deployTomcat function for WAR deployment
    Added deployTomcat function to transfer WAR file.

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -8,5 +8,16 @@ def buildArtifact()
   sh 'mvn package'
 }
 
+def deployTomact(jobname,ip,context)
+{
+  sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat10/webapps/${context}"    
+}              
 
 
+
+  
+
+
+
+     
+               
