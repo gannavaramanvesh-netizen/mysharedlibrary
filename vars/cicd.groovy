4686f3136a45967251cd917c6a0e723939cbbf47
gannavaramanvesh-netizen <gannavaramanvesh@gmail.com>
2025-10-18 17:41:16 +0530

    Add executeselenium function to cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -11,7 +11,12 @@ def buildArtifact()
 def deployTomact(jobname,ip,context)
 {
   sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat10/webapps/${context}"    
-}              
+}       
+
+def executeselenium(jobname)
+{
+ sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
+}
 
 
 
