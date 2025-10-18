fcc4e65175bc066faef1bf98b47a7132868839af
gannavaramanvesh-netizen <gannavaramanvesh@gmail.com>
2025-10-18 16:58:46 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -2,3 +2,12 @@ def gitdownload(repo)
 {
   git "https://github.com/gannavaramanvesh-netizen/${repo}.git"
 }
+
+def buildArtifact()
+{
+sh 'mvn package'
+}
+def deploy("job name","ip_address","context path")
+{
+
+}
